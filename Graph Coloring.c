9e4b4fa3f1989e1cb3eb6e74da1123479d4af599
soumimoana <81809794+soumimoana@users.noreply.github.com>
2022-07-22 11:13:14 +0530

    Create Graph Coloring.c

---
+++ Graph Coloring.c
@@ -0,0 +1,56 @@
+ #include<stdio.h>
+int G[50][50],x[50];  //G:adjacency matrix,x:colors
+void next_color(int k){
+   int i,j;
+   x[k]=1;  //coloring vertex with color1
+   for(i=0;i<k;i++){ //checking all k-1 vertices-backtracking
+     if(G[i][k]!=0 && x[k]==x[i])  //if connected and has same color
+       x[k]=x[i]+1;  //assign higher color than x[i]
+   }
+}
+
+int main(){
+  int n,e,i,j,k,l;
+  printf("Enter no. of vertices : ");
+  scanf("%d",&n);  //total vertices
+  printf("Enter no. of edges : ");
+  scanf("%d",&e);  //total edges
+ 
+  for(i=0;i<n;i++)
+    for(j=0;j<n;j++)
+      G[i][j]=0;  //assign 0 to all index of adjacency matrix
+     
+  printf("Enter indexes where value is 1-->\n");
+  for(i=0;i<e;i++){
+    scanf("%d %d",&k,&l);
+    G[k][l]=1;
+    G[l][k]=1;
+  }
+ 
+  for(i=0;i<n;i++)
+    next_color(i);  //coloring each vertex
+
+  printf("Colors of vertices -->\n");
+  for(i=0;i<n;i++)  //displaying color of each vertex
+    printf("Vertex[%d] : %d\n",i+1,x[i]);
+
+  return 0;
+}
+
+-------------------------------------------------------
+  OUTPUT
+  
+  
+  Enter no. of vertices : 4
+Enter no. of edges : 5
+Enter indexes where value is 1-->
+0 1
+1 2
+1 3
+2 3
+3 0
+Colors of vertices -->
+Vertex[1] : 1
+Vertex[2] : 2
+Vertex[3] : 1
+Vertex[4] : 3
